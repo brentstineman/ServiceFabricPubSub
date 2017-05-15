120b2ae7cc66393c7e28516903ebfa8f899b920f
Brent Stineman <brent.stineman@microsoft.com>
2017-05-15 11:46:19 -0700

    forgot some files

--- src/ServiceFabricPubSub/Administration/Controllers/ValuesController.cs
+++ src/ServiceFabricPubSub/Administration/Controllers/ValuesController.cs
@@ -4,31 +4,32 @@ using System.Web.Http;
 namespace Administration.Controllers
 {
     [ServiceRequestActionFilter]
-    public class ValuesController : ApiController
+    public class TenantsController : ApiController
     {
-        // GET api/values 
+        // GET api/tenants 
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5 
+        // GET api/tenants/5 
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values 
+        // POST api/tenants 
         public void Post([FromBody]string value)
         {
+            // creates a new instance of the tenant application
         }
 
-        // PUT api/values/5 
+        // PUT api/tenants/5 
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5 
+        // DELETE api/tenants/5 
         public void Delete(int id)
         {
         }
