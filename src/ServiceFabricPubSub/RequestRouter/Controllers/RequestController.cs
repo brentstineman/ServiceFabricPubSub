af1701b724d7ff91104f46d41bc1d2916df6bcff
Michael S. Collier <buckeye01@hotmail.com>
2017-05-15 15:51:37 -0700

    Stubbed out some very rough logic for authenticating the request.

--- src/ServiceFabricPubSub/RequestRouter/Controllers/RequestController.cs
+++ src/ServiceFabricPubSub/RequestRouter/Controllers/RequestController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -11,31 +12,50 @@ namespace RequestRouterService.Controllers
         // PUT api/tenantId/topicName
         public async Task<HttpResponseMessage> Put(string tenantId, string topicName)
         {
-            HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.Accepted);
+            HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.InternalServerError);
             string partitionKey = string.Empty;
 
             // TODO: Parse the partition key from the body or the input param?
             string messageBody = await this.Request.Content.ReadAsStringAsync();
 
-            // TODO: Authenticate the request using the Administration Services
+            bool isAuthenticated = await AuthenticateRequest();
 
-
-            // TODO: Call the Topic Service to post the message . . .use the Reverse Proxy.
-
-            var response = await InvokeService("TenantApplication", "TopicService", partitionKey);
-            if (response != null && response.IsSuccessStatusCode)
+            if (isAuthenticated)
             {
-                var msg = await response.Content.ReadAsStringAsync();
+                // Call the Topic Service to post the message.
+                var response = await InvokeService("TenantApplication", "TopicService", string.Empty, partitionKey);
+                if (response != null && response.IsSuccessStatusCode)
+                {
+                    responseMessage.StatusCode = HttpStatusCode.Accepted;
 
-                // TODO: do something with the response.
-            }
-            else
-            {
-                responseMessage.StatusCode = HttpStatusCode.InternalServerError;
-                responseMessage.ReasonPhrase = response?.ReasonPhrase ?? "Internal error";
+                    var msg = await response.Content.ReadAsStringAsync();
+
+                    // TODO: do something with the response.
+                }
+                else
+                {
+                    responseMessage.StatusCode = HttpStatusCode.InternalServerError;
+                    responseMessage.ReasonPhrase = response?.ReasonPhrase ?? "Internal error";
+                }
             }
 
             return responseMessage;
+        }
+
+        private async Task<bool> AuthenticateRequest()
+        {
+            bool isAuthenticated = false;
+
+            // TODO: Call the real service.
+
+
+            var response = await InvokeService("FrontEnd", "Administration", string.Empty, string.Empty);
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                isAuthenticated = true;
+            }
+
+            return isAuthenticated;
         }
 
         // GET api/tenantId/topicName
@@ -44,7 +64,7 @@ namespace RequestRouterService.Controllers
             return new HttpResponseMessage(HttpStatusCode.Accepted);
         }
 
-        private static async Task<HttpResponseMessage> InvokeService(string appName, string serviceName, string partitionKey)
+        private static async Task<HttpResponseMessage> InvokeService(string appName, string serviceName, string path, string partitionKey)
         {
             HttpResponseMessage responseMessage;
             try
