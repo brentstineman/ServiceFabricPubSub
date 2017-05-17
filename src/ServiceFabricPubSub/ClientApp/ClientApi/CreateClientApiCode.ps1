7fa08f01300a6a2af05aca98b8820258d762c649
DX Italia <dxitalia@outlook.com>
2017-05-17 12:34:45 -0700

    Added autorest folder

---
+++ src/ServiceFabricPubSub/ClientApp/ClientApi/CreateClientApiCode.ps1
@@ -0,0 +1,14 @@
+﻿$autorestExe= "C:\Users\gianlucb\AppData\Roaming\npm\autorest.cmd";
+$swaggerFile = "swagger_api.json";
+$outputCSFile =  "ClientApi.cs";
+
+If (Test-Path $outputCSFile){
+	Remove-Item $outputCSFile
+}
+
+If (Test-Path $swaggerFile){
+	Remove-Item $swaggerFile
+}
+
+Invoke-WebRequest -Uri http://localhost:8979/swagger/docs/v3 -OutFile $swaggerFile;
+Start-Process -FilePath $autorestExe -NoNewWindow -ArgumentList "--input-file=$swaggerFile --csharp --output-folder=. --namespace=ClientApi" -RedirectStandardOutput log.txt -RedirectStandardError logError.txt
