d25bc07006098f1528d419455ae32d6ca0031325
Nicolas CLERC <nicolas.clerc@sablefin.net>
2017-05-17 15:31:59 -0700

    implementing duplicate message check in subscriber

--- src/ServiceFabricPubSub/PubSubDotnetSDK/PubSubMessage.cs
+++ src/ServiceFabricPubSub/PubSubDotnetSDK/PubSubMessage.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace PubSubDotnetSDK
 {
+    /// <summary>
+    /// Internal class used to persist message in ServiceFabric queue
+    /// </summary>
     [DataContract]
     public class PubSubMessage
     {
@@ -13,6 +16,17 @@ namespace PubSubDotnetSDK
             this.Message = null;
         }
 
+
+        /// <summary>
+        /// unique Message ID set by the TopicService when message is received.
+        /// used to check duplicate in subscriber.
+        /// </summary>
+        [DataMember]
+        public Guid MessageID { get; set; } 
+
+        /// <summary>
+        /// Message content/payload
+        /// </summary>
         [DataMember]
         public string Message { get; set; }
     }
